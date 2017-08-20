81cf4700dbd9f4daec2b5d0c302f692be0ae1477
George Stavrinos <gstavrinos@iit.demokritos.gr>
2017-08-20 15:57:45 +0300

    tf and marker visualization working as expected! :beers:

--- scripts/ar_helipad.py
+++ scripts/ar_helipad.py
@@ -1,66 +1,87 @@
 #!/usr/bin/env python
+import tf
 import math
 import rospy
 import untangle
+from tf2_msgs.msg import TFMessage
 from visualization_msgs.msg import Marker
 from ar_track_alvar_msgs.msg import AlvarMarkers
 
-marker_publisher = None
 width = 0.0
 height = 0.0
 diagonal = 0.0
+top_left_x = 0
+top_left_y = 0
+master_marker = '0'
+tf_broadcaster = None
+marker_publisher = None
 
 def init():
-    global marker_publisher
-    global width, height, diagonal
+    global marker_publisher, master_marker, tf_broadcaster
+    global width, height, diagonal, top_left_x, top_left_y
     rospy.init_node('ar_helipad')
     xml = rospy.get_param("~xml_file", "bundles/helipad1.xml")
     xml_file = untangle.parse(xml)
+    master_marker = xml_file.multimarker.marker[0]['index']
     top_left_x = float(xml_file.multimarker.marker[0].corner[3]['x'])
     top_left_y = float(xml_file.multimarker.marker[0].corner[3]['y'])
     bottom_right_x = float(xml_file.multimarker.marker[1].corner[1]['x'])
     bottom_right_y = float(xml_file.multimarker.marker[1].corner[1]['y'])
-    width = bottom_right_x + abs(top_left_x)
-    height = abs(bottom_right_y + top_left_y)
+    width = abs(bottom_right_x) + abs(top_left_x)
+    height = abs(bottom_right_y) + abs(top_left_y)
     diagonal = math.sqrt(width**2 + height**2)
     print 'The helipad has a width of ' + str(width) + 'cm.'
     print 'The helipad has a height of ' + str(height) + 'cm.'
     print 'The helipad has a diagonal of ' + str(diagonal) + 'cm.'
     width /= 100
     height /= 100
+    top_left_x /= 100
+    top_left_y /= 100
     diagonal /= 100
+    tf_broadcaster = tf.TransformBroadcaster()
     marker_publisher = rospy.Publisher('ar_helipad_pose', Marker, queue_size=1)
-    rospy.Subscriber("ar_pose_marker", AlvarMarkers, ar_callback)
+    rospy.Subscriber("tf", TFMessage, tf_callback)
     while not rospy.is_shutdown():
         rospy.spin()
 
-def ar_callback(msg):
-    global marker_publisher
-    global width, height, diagonal
-    marker = Marker()
-    if len(msg.markers) > 0:
-        marker.header.frame_id = msg.markers[0].header.frame_id
-        marker.header.stamp = rospy.Time().now()
-        marker.ns = 'ar_helipad'
-        marker.id = 0
-        # Based on http://docs.ros.org/api/visualization_msgs/html/msg/Marker.html
-        # The shape code for the cube (rectangle) is 1
-        # The add/modify action is 0
-        marker.type = 1
-        marker.action = 0
-        marker.color.r = 1.0
-        marker.color.g = 1.0
-        marker.color.b = 0.0
-        marker.color.a = 1.0
-        marker.scale.x = width
-        marker.scale.y = height
-        marker.scale.z = 0.001
-        marker.pose = msg.markers[0].pose.pose
-        #marker.pose.position.x = msg.markers[0].pose.pose.position.x + width / 2
-        #marker.pose.position.y = msg.markers[0].pose.pose.position.y - diagonal / 2
-        marker.lifetime = rospy.Duration(1);
-        marker_publisher.publish(marker)
-    #print msg
+def tf_callback(tf2):
+    global master_marker, tf_broadcaster
+    global diagonal, width, height
+    for tf in tf2.transforms:
+        if tf.child_frame_id == 'ar_marker_' + master_marker:
+            tf_broadcaster.sendTransform(
+                (width / 2 + top_left_x, -height / 2 + top_left_y, 0),
+                (0, 0, 0, 1.0),
+                rospy.Time.now(),
+                'helipad',
+                'ar_marker_' + master_marker)
+
+            marker = Marker()
+            marker.header.frame_id = 'ar_marker_' + master_marker
+            marker.header.stamp = rospy.Time.now()
+            marker.ns = 'ar_helipad'
+            marker.id = 0
+            # Based on http://docs.ros.org/api/visualization_msgs/html/msg/Marker.html
+            # The shape code for the cube (rectangle) is 1
+            # The add/modify action is 0
+            marker.type = 1
+            marker.action = 0
+            marker.color.r = 1.0
+            marker.color.g = 1.0
+            marker.color.b = 0.0
+            marker.color.a = 1.0
+            marker.scale.x = width
+            marker.scale.y = height
+            marker.scale.z = 0.001
+            marker.pose.position.x = width / 2 + top_left_x
+            marker.pose.position.y = -height / 2 + top_left_y
+            marker.pose.position.z = 0
+            marker.pose.orientation.x = 0
+            marker.pose.orientation.y = 0
+            marker.pose.orientation.z = 0
+            marker.pose.orientation.w = 1.0
+            marker.lifetime = rospy.Duration(1);
+            marker_publisher.publish(marker)
 
 if __name__ == '__main__':
     init() 
